000e55952deffee4b4b3c374b7c54f8f7d8f38f3
Fernando <mhfernando778@gmail.com>
2025-02-21 23:29:07 -0500

    Task 2 - added comments

--- cc_9.js
+++ cc_9.js
@@ -25,22 +25,22 @@ console.log(emp1.calculateAnnualSalary()); // logging the employee salary calcul
 
 // Task 2 - Creating Manager Class Inheritance //
 
-class Manager extends Employee {
+class Manager extends Employee { // creating a manager class that will inherit the employee class
     constructor(name, id, department, salary, teamSize) {
         super(name, id, department, salary);
-        this.teamSize = teamSize;
+        this.teamSize = teamSize; // adding a new teamSize property
     };
 
-    getDetails() {
+    getDetails() { // overriding the method to inclide team size
         return `Manager Name: ${this.name}, Manager ID: ${this.name}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`
     };
 
-    calculateBonus() {
+    calculateBonus() { // calculating the manager annual bonus from their salary
         return (this.salary * 12) * .10;
     };
 };
 
-const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5); // introducing a new manager using the class
 
-console.log(mgr1.getDetails());
-console.log(mgr1.calculateBonus()); 
+console.log(mgr1.getDetails()); // logging the manager details
+console.log(mgr1.calculateBonus()); // logging the manager annual bonus
