ab7fb7b66bf4dfb2804ccaa94b1c3b19f3f468cb
Fernando <mhfernando778@gmail.com>
2025-02-21 21:09:52 -0500

    Task 1 - added comments

--- cc_9.js
+++ cc_9.js
@@ -1,6 +1,6 @@
 // Task 1 - Creating Employee Class //
 
-class Employee {
+class Employee { // creating a class with employee information
     constructor(name, id, department, salary) {
         this.name = name;
         this.id = id;
@@ -8,16 +8,16 @@ class Employee {
         this.salary = salary;
     }
 
-    getDetails() {
+    getDetails() { // adding a method to return employee details
         return `Employee Name: ${this.name}, Employee ID: ${this.id}, Employee Department: ${this.department}, Employee Salary: ${this.salary}`
     };
 
-    calculateAnnualSalary() {
+    calculateAnnualSalary() { // adding a method to return employee salary calculation
         return this.salary * 12
     };
 }
 
-const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000); // introducing a new employee using the class
 
-console.log(emp1.getDetails());
-console.log(emp1.calculateAnnualSalary()); 
+console.log(emp1.getDetails()); // logging the employee details
+console.log(emp1.calculateAnnualSalary()); // logging the employee salary calculation
